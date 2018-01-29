e2354ef808cf3bcf9b4beeb8dd808b792f35747a
Visdroid <aldrinvisser@gmail.com>
2018-01-30 00:21:54 +0200

    MVC Test

---
+++ Jeudfra-Beta/Controllers/PolicyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jeudfra_Beta.Models;
+
+namespace Jeudfra_Beta.Controllers
+{
+    public class PolicyController : Controller
+    {
+        // GET: Policy
+        public ActionResult Random()
+        {
+            var policy = new Policy() {Name = "Standard"};
+
+            return View(policy);
+        }
+    }
+}
---
+++ Jeudfra-Beta/Models/Policy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jeudfra_Beta.Models
+{
+    public class Policy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
